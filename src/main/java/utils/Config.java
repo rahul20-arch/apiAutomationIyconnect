88d1f0720de734312e27810a1f036e4c4224cb23
rahul20-arch <sivapalanrahul@gmail.com>
2026-03-26 17:34:24 +0530

    secondCommit

--- src/main/java/utils/Config.java
+++ src/main/java/utils/Config.java
@@ -1,6 +1,6 @@
 package utils;
 
 public class Config {
-	public static final String BASE_URL = "http://20.205.184.152:8080/api";
+	public static final String BASE_URL = "http://20.205.184.152:3000/api";
     public static final String LOGIN_ENDPOINT = "/auth/login";
 }
